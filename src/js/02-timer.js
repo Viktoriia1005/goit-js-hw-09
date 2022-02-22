a9f91ba6fedb646a73b2ce92300a467450593fe2
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-23 00:50:48 +0200

    add variables in task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,17 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  input: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+const isDisabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
