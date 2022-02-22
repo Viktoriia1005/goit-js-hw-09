4b7a5c4d9d34a1dba83be52788111c2705160e07
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-22 22:47:50 +0200

    done task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.body;
+const startBtn = document.querySelector('[data-start');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+const isDisabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartBtnClick() {
+  timerId = setInterval(changeBgColor, 1000);
+  startBtn.disabled = isDisabled;
+  stopBtn.disabled = !isDisabled;
+}
+startBtn.addEventListener('click', onStartBtnClick);
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  stopBtn.disabled = isDisabled;
+  startBtn.disabled = !isDisabled;
+}
+stopBtn.addEventListener('click', onStopBtnClick);
